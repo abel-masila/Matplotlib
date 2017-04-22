f0ed49ee5d5f9d6f2be1601ce4ddae775112580a
Abel <abelmasila@gmail.com>
2017-04-22 16:10:17 +0300

    added a legend

--- titles.py
+++ titles.py
@@ -1,8 +1,15 @@
 import matplotlib.pyplot as plt
+
 x=[1,2,4]
 y=[5,7,4]
-plt.plot(x,y)
+x2=[1,2,3]
+y2=[10,16,12]
+
+plt.plot(x,y, label='First Line')
+plt.plot(x2,y2, label='Second Line')
+
 plt.xlabel("Plot Number")
 plt.ylabel("Important var")
 plt.title("New Graph")
+plt.legend()
 plt.show()
